02bb0f60bddc7affc1ce632ba0793a56cd9f9eb1
Dylan Esterhuizen <u23668475@tuks.co.za>
2025-03-19 19:31:48 +0200

    Add unit tests for productController and setup test project
    Introduced a new test project and added unit tests for the
    productController class. Updated solution and project files
    to include necessary references and configurations.
    
    - Added using directives in productControllerTests.cs for Moq,
      Microsoft.AspNetCore.Mvc, and project controllers/models.
    - Created test method getProduct_ReturnsNotFound_WhenProductDoesNotExist.
    - Updated u23668475_HW01.Tests.csproj with package references for
      Microsoft.AspNetCore.Mvc and Moq.
    - Added new project entry for u23668475_HW01_Tests in u23668475_HW01.sln.
    - Created UnitTest1.cs with a basic unit test class and placeholder method.
    - Created u23668475_HW01_Tests.csproj with properties and package
      references for coverlet.collector, Microsoft.NET.Test.Sdk, xunit,
      and xunit.runner.visualstudio.

--- u23668475_HW01.Tests/Controller/productControllerTests.cs
+++ u23668475_HW01.Tests/Controller/productControllerTests.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using u23668475_HW01.Controllers;
+using u23668475_HW01.Models;
 
 namespace u23668475_HW01.Tests
 {
     internal class productControllerTests
     {
+        [Fact]
+
+        public async Task getProduct_ReturnsNotFound_WhenProductDoesNotExist()
+        {
+            var mockRepo = new Mock<IproductRepository>
+        }
 
     }
 }
---
+++ u23668475_HW01_Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace u23668475_HW01_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
