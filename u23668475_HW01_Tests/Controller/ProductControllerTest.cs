a285a2ca38ceba338b69e4b3b49a2727a85b5ba4
Dylan Esterhuizen <u23668475@tuks.co.za>
2025-03-20 12:42:16 +0200

    Add unit test for ProductController and update test project
    Introduced a new unit test `GetProduct_ReturnsNotFound_WhenProductDoesNotExist` in `ProductControllerTest.cs` to verify that `getProduct` returns `NotFoundResult` for non-existent products. Utilized Moq library to mock `IProductRepository`.
    
    Updated `u23668475_HW01_Tests.csproj` to include Moq library and added a project reference to `u23668475_HW01.csproj` for accessing main project classes and interfaces.

---
+++ u23668475_HW01_Tests/Controller/ProductControllerTest.cs
@@ -0,0 +1,25 @@
+﻿
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using u23668475_HW01.Controllers;
+using u23668475_HW01.Models;
+using u23668475_HW01.Repositories;
+
+namespace u23668475_HW01_Tests.Tests
+{
+    public class ProductControllerTest
+    {
+        [Fact]
+        public async Task GetProduct_ReturnsNotFound_WhenProductDoesNotExist()
+        {
+            var mockRepo = new Mock<IproductRepository>();
+            mockRepo.Setup(repo => repo.getProductsById(1)).ReturnsAsync((Product)null);
+
+            var controller = new productController(mockRepo.Object);
+            var result = await controller.getProduct(1);
+
+            Assert.IsType<NotFoundResult>(result.Result);  
+        }
+
+    }
+}
